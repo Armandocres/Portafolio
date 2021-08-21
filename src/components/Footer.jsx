f55a31c2e2e45b97f358884ede691d20c7f988f7
Armandocres <64869199+Armandocres@users.noreply.github.com>
2021-08-21 12:17:55 -0500

    Arreglando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/components/footer.scss";
 const Footer = () => {
 	return (
 		<footer className="Footer">
-			<span className="Footer__socials">Armando.cruzes@gmail.com</span>
+			<span className="Footer__socials">armando.cruzes@hotmail.com</span>
 			<span className="Footer__socials">
 				<a href="https://drive.google.com/file/d/18LY3zwywRl0KRm-s6joMW18omNpsakLU/view?usp=sharing">
 					Curriculum
