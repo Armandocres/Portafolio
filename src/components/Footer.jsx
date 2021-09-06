e14fb1064aed7c7ef435ecc0509041f0c5bd66a2
Armandocres <64869199+Armandocres@users.noreply.github.com>
2021-09-06 12:25:41 -0500

    Arreglando contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/components/footer.scss";
 const Footer = () => {
 	return (
 		<footer className="Footer">
-			<span className="Footer__socials">armando.cruzes@hotmail.com</span>
+			<span className="Footer__socials">armandocres99@gmail.com</span>
 			<span className="Footer__socials">
 				<a href="https://drive.google.com/file/d/18LY3zwywRl0KRm-s6joMW18omNpsakLU/view?usp=sharing">
 					Curriculum
